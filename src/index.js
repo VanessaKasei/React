a447d339ab6e90106d1597f7f493a577c7ec39fc
VanessaKasei <kaseivanessa@gmail.com>
2023-08-23 21:41:17 +0300

    React Render HTML

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// create root() - defines a the html element where a React component should be displayed
+//render - defines the React component that should be rendered
+//JSX allows us to write html tags inside js code
 
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const myTrials = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Mary</td>
+    </tr>
+  </table>
 );
+
+const example1 = document.getElementById("demo");
+const demo = ReactDOM.createRoot(example1);
+demo.render(myTrials)
+
