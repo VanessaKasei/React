d6cf03722afcb83474e6debe2ee6f8bbe5873d3c
VanessaKasei <kaseivanessa@gmail.com>
2023-08-23 23:58:19 +0300

    jsx if statement

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom/client';
 // create root() - defines a the html element where a React component should be displayed
 //render - defines the React component that should be rendered
 //JSX allows us to write html tags inside js code
+//root node is the html element where you want to display the results
+const x = 5;
+let text = "Goodbye";
+if (x < 10) {
+  text = "Hello";
+}
 
-const myTrials = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Mary</td>
-    </tr>
-  </table>
-);
+const myElement = <h1>{text}</h1>;
 
-const example1 = document.getElementById("demo");
-const demo = ReactDOM.createRoot(example1);
-demo.render(myTrials)
-
+const root = ReactDOM.createRoot(document.getElementById('demo'));
+root.render(myElement);
