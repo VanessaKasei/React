84c0c18dae4db35cf1fc38688fd0661b7cbd982a
VanessaKasei <kaseivanessa@gmail.com>
2023-08-30 01:10:16 +0300

    navbar and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Layout />}/>
+          <Route index element={<Home />}/>
+          <Route path='about' element={<About />} />
+          <Route path='services' element={<Services />}/>
+          <Route path='contact' element={<Contact/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
