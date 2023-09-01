2301f1876501f14cbac8f5e3824b97c0c3aaf3ee
VanessaKasei <kaseivanessa@gmail.com>
2023-09-01 21:38:44 +0300

    image in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
-const Home = () => {
-  return <h1>Home</h1>;
-};
+import React from "react";
+import home1 from "./images/home1.jpg";
+import './Home.css'
+ 
+function Home() {
+  return (
+    <div className="image-container">
+      <img src={home1} alt="a beautiful home" />
+      <div className="overlay-text">Your Dream Home Finder</div>
+    </div>
+  );
+}
 
 export default Home;
