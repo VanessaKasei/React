73c6b715fb09ce556537afc9fc18e7c48efe3742
VanessaKasei <kaseivanessa@gmail.com>
2023-09-20 00:00:11 +0300

    home listing props

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,16 @@
+import React from "react";
+import Contact from "./Contact";
+import properties from "../data";
+import "./Services.css"; 
+
 const Services = () => {
-  return <h1>Services</h1>;
+  return (
+    <div className="property-listing">
+      {properties.map((property) => (
+        <Contact key={property.id} property={property} />
+      ))}
+    </div>
+  );
 };
 
 export default Services;
