5e804fa9fe16454b7da1070e1566264363d91e64
VanessaKasei <kaseivanessa@gmail.com>
2023-09-19 23:59:46 +0300

    navbar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,19 @@
-const Contact = () => {
-  return <h1>Contact Me</h1>;
-};
+import React from "react";
+import "./Contact.css";
 
-export default Contact;
+
+
+
+const Contact = ({property}) => {
+  return (
+    <div className="property-card">
+      {property && property.image && (<img src={property.image} alt="a beautiful house" />)}
+      {property && property.title && <h1>{property.title}</h1>}
+      {property && property.location &&<p>{property.location}</p>}
+      {property && property.price &&<p>Price: {property.price}</p>}
+      {property && property.bedrooms &&<p>Bedrooms: {property.bedrooms}</p>}
+      {property && property.bathrooms &&<p>Bathrooms: {property.bathrooms}</p>}
+    </div>
+  );
+};
+export default Contact;   
