b4f8afaa1e2d492e737f711cba4f04c20b1e255c
VanessaKasei <kaseivanessa@gmail.com>
2023-09-22 21:54:35 +0300

    home data to be displayed

---
+++ src/pages/data.js
@@ -0,0 +1,68 @@
+const properties = [
+    {
+      id: 1,
+      title: 'Home1',
+      price: '$250,000',
+      location: '1234 Elm Street, Springfield',
+      bedrooms: 3,
+      bathrooms: 2,
+      image: '/images/bungalow3.jpg'
+    },
+    {
+      id: 2,
+      title: 'Home2',
+      price: '$150,000',
+      location: '5678 Oak Avenue, Maplewood',
+      bedrooms: 2,
+      bathrooms: 1,
+      image: '/images/bungalow3.jpg'
+    },
+    {
+      id: 3,
+      title: 'Home3',
+      price: '$250,000',
+      location: '1234 Elm Street, Springfield',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/bungalow2.jpg'
+    },
+    {
+      id: 4,
+      title: 'Home4',
+      price: '$150,000',
+      location: '5678 Oak Avenue, Maplewood',
+      bedrooms: 2,
+      bathrooms: 1,
+      image: '/images/bungalow2.jpg'
+    },
+    {
+      id: 5,
+      title: 'Home5',
+      price: '$250,000',
+      location: '1234 Elm Street, Springfield',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/bungalow3.jpg'
+    },
+    {
+      id: 6,
+      title: 'Home6',
+      price: '$150,000',
+      location: '5678 Oak Avenue, Maplewood',
+      bedrooms: 2,
+      bathrooms: 1,
+      image: '/images/bungalow2.jpg'
+    },
+    {
+      id: 7,
+      title: 'Home7',
+      price: '$250,000',
+      location: '1234 Elm Street, Springfield',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/bungalow3.jpg'
+    },
+
+  ];
+  
+  export default properties;
