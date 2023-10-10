25d03058d6d7c73efe441bf8d7e2d10af1b9f8ce
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 14:14:35 +0300

    use state for navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -10,6 +10,6 @@ const Layout = ({ children }) => {
       </main>  
     </div>
   );
-};
+}
 
 export default Layout;
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,26 +1,50 @@
-import React from 'react';
+// src/Navbar.js
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to = "/">Home</Link>
-                </li>
-                <li>
-                    <Link to = "/about">About</Link>
-                </li>
-                <li>
-                    <Link to = "/services">Services</Link>
-                </li>
-                <li>
-                    <Link to = "/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-    );
-};
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowMenu(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <nav className={`navbar ${showMenu ? 'show-menu' : ''}`}>
+      <div className='container'>
+        {showMenu ? (
+          <ul className='nav-list'>
+            <li className='nav-item'>
+              <Link to="/">Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/homelisting">Home Listing</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        ) : (
+          <div className='hamburger-icon' onClick={toggleMenu}>
+            &#9776; {/* Hamburger icon */}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
