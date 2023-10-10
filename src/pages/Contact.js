b68218be9ef7dea965307121f6e8530b34d64297
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 14:12:15 +0300

    added and styled contact section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,26 @@
 import React from "react";
-import "./Contact.css";
+import "./Contact.css"
 
+function Contact(){
+    return(
+        <div className="contact-page">
+            <h1>Let's Connect!</h1>
+            <div id="myicons">
+                <i className='fa fa-facebook' id="facebook-icon"> </i>
+                <a href="https://api.whatsapp.com/send?phone=+254710705715" target="_blank" rel="noopener noreferrer"> 
+                <i className='fa fa-whatsapp' id="whatsapp-icon"></i>
+                </a> 
+                <a href='https://instagram.com/_uprime?igshid=OGQ5ZDc2ODk2ZA==' target="_blank" rel="noopener noreferrer">
+                <i className='fa fa-instagram' id="instagram-icon"></i>
+                </a>
+            </div>
+            <div className='contact-container'>
+                <p id='contact'>CONTACT US TODAY:  
+                <i className='fa fa-phone' id='phone'>  +254 701353 971</i>
+                <i className='fa fa-envelope' id='mail'> :mwayiupright@gmail.com</i></p>
+            </div>
+        </div>
+    );
+}
 
-
-
-const Contact = ({property}) => {
-  return (
-    <div className="property-card">
-      {property && property.image && (<img src={property.image} alt="a beautiful house" />)}
-      {property && property.title && <h1>{property.title}</h1>}
-      {property && property.location &&<p>{property.location}</p>}
-      {property && property.price &&<p>Price: {property.price}</p>}
-      {property && property.bedrooms &&<p>Bedrooms: {property.bedrooms}</p>}
-      {property && property.bathrooms &&<p>Bathrooms: {property.bathrooms}</p>}
-    </div>
-  );
-};
-export default Contact;   
+export default Contact;
