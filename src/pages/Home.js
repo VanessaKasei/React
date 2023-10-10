cb043306be4c801f71c43be797d92589f9768146
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 14:13:30 +0300

    edit testominials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,17 +12,16 @@ function Home() {
         {/*<div className="overlay-text">Your Dream Home Finder</div>*/}
       </div>
       <div className="carousel-container">
-      <Carousel autoPlay={true} infiniteLoop={true} interval={3000} showStatus={false} showThumbs={false}>
+      <Carousel className="carousel" autoPlay={true} infiniteLoop={true} interval={4000} showStatus={false} showThumbs={false}>
         <div>
           <p>This is the best home listing website to ever exist</p>
         </div>
         <div>
-          <p>I found my dream home easilyt thanks to this website</p>
+          <p>I found my dream home easily thanks to this website</p>
         </div>
         <div>
-          <p>No othe website beats this one. It is the perfect one for
-            anyone looking for their dream home.
-          </p>
+          <p>One of the best websites. It is the perfect one for
+            anyone looking for their dream home.</p>
         </div>
         {/* Add more carousel items with text as needed */}
       </Carousel>
