46ac255c226473eb1d2d268e0082f152a948ebb8
VanessaKasei <kaseivanessa@gmail.com>
2023-10-16 00:59:29 +0300

    removed useeffect hook

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,31 +1,11 @@
 // src/Navbar.js
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-  const [showMenu, setShowMenu] = useState(window.innerWidth > 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setShowMenu(window.innerWidth > 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
   return (
-    <nav className={`navbar ${showMenu ? 'show-menu' : ''}`}>
-      <div className='container'>
-        {showMenu ? (
+    <div className='navbar'>
           <ul className='nav-list'>
             <li className='nav-item'>
               <Link to="/">Home</Link>
@@ -36,14 +16,8 @@ function Navbar() {
             <li className='nav-item'>
               <Link to="/contact">Contact</Link>
             </li>
-          </ul>
-        ) : (
-          <div className='hamburger-icon' onClick={toggleMenu}>
-            &#9776; {/* Hamburger icon */}
-          </div>
-        )}
-      </div>
-    </nav>
+          </ul> 
+          </div>     
   );
 }
 
