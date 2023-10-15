5f6069fba139e4a6dea9495a8be6dac2e9dad66e
VanessaKasei <kaseivanessa@gmail.com>
2023-10-16 00:21:05 +0300

    changed locations

--- src/pages/data.js
+++ src/pages/data.js
@@ -3,66 +3,137 @@ const properties = [
       id: 1,
       title: 'Home1',
       price: '$250,000',
-      location: '1234 Elm Street, Springfield',
+      location: 'Embakasi',
       bedrooms: 3,
       bathrooms: 2,
-      image: '/images/bungalow3.jpg'
+      image: '/images/home10.jpg'
     },
     {
       id: 2,
       title: 'Home2',
       price: '$150,000',
-      location: '5678 Oak Avenue, Maplewood',
+      location: 'Ngong',
       bedrooms: 2,
       bathrooms: 1,
-      image: '/images/bungalow3.jpg'
+      image: '/images/home11.jpg'
     },
     {
       id: 3,
       title: 'Home3',
       price: '$250,000',
-      location: '1234 Elm Street, Springfield',
+      location: 'Kitisuru',
       bedrooms: 3,
       bathrooms: 2,
-     image: '/images/bungalow2.jpg'
+     image: '/images/home12.jpg'
     },
     {
       id: 4,
       title: 'Home4',
       price: '$150,000',
-      location: '5678 Oak Avenue, Maplewood',
+      location: 'Kitengela',
       bedrooms: 2,
       bathrooms: 1,
-      image: '/images/bungalow2.jpg'
+      image: '/images/home13.jpg'
     },
     {
       id: 5,
       title: 'Home5',
       price: '$250,000',
-      location: '1234 Elm Street, Springfield',
+      location: 'Donholm',
       bedrooms: 3,
       bathrooms: 2,
-     image: '/images/bungalow3.jpg'
+     image: '/images/home14.jpg'
     },
     {
       id: 6,
       title: 'Home6',
       price: '$150,000',
-      location: '5678 Oak Avenue, Maplewood',
+      location: 'Embakasi',
       bedrooms: 2,
       bathrooms: 1,
-      image: '/images/bungalow2.jpg'
+      image: '/images/home15.jpg'
     },
     {
       id: 7,
       title: 'Home7',
       price: '$250,000',
-      location: '1234 Elm Street, Springfield',
+      location: 'Runda',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/bungalow2.jpg'
+    },
+    {
+      id: 8,
+      title: 'Home8',
+      price: '$250,000',
+      location: 'Kileleshwa',
       bedrooms: 3,
       bathrooms: 2,
      image: '/images/bungalow3.jpg'
     },
-
+    {
+      id: 9,
+      title: 'Home9',
+      price: '$250,000',
+      location: 'Ngara',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home9.jpg'
+    },
+    {
+      id: 10,
+      title: 'Home10',
+      price: '$250,000',
+      location: 'Runda',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home8.webp'
+    },
+    {
+      id: 11,
+      title: 'Home11',
+      price: '$250,000',
+      location: 'Westlands',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home7.webp'
+    },
+    {
+      id: 12,
+      title: 'Home12',
+      price: '$250,000',
+      location: 'Parklands',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home6.jpg'
+    },
+    {
+      id: 13,
+      title: 'Home13',
+      price: '$250,000',
+      location: 'kileleshwa',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home5.webp'
+    },
+    {
+      id: 14,
+      title: 'Home14',
+      price: '$250,000',
+      location: 'Upperhill',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home4.jpg'
+    },
+    {
+      id: 15,
+      title: 'Home15',
+      price: '$250,000',
+      location: 'Kilimani',
+      bedrooms: 3,
+      bathrooms: 2,
+     image: '/images/home3.jpg'
+    },
   ];
   
   export default properties;
