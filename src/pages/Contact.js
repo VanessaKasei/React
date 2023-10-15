76881c7632b447a1fa0a2882153ac10dc2905eb8
VanessaKasei <kaseivanessa@gmail.com>
2023-10-16 00:20:46 +0300

    added social media icons

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,26 @@
 import React from "react";
-import "./Contact.css"
-
-function Contact(){
-    return(
-        <div className="contact-page">
-            <h1>Let's Connect!</h1>
-            <div id="myicons">
-                <i className='fa fa-facebook' id="facebook-icon"> </i>
-                <a href="https://api.whatsapp.com/send?phone=+254710705715" target="_blank" rel="noopener noreferrer"> 
-                <i className='fa fa-whatsapp' id="whatsapp-icon"></i>
-                </a> 
-                <a href='https://instagram.com/_uprime?igshid=OGQ5ZDc2ODk2ZA==' target="_blank" rel="noopener noreferrer">
-                <i className='fa fa-instagram' id="instagram-icon"></i>
+import './Contact.css'
+function Contact () {
+    return (
+        <section id="contact">
+            <h1 id="contact-info">Let's connect!</h1>
+            <div id="myicons">    
+                <a href="https://api.whatsapp.com/send?phone=+254710705715" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-whatsapp" id="whatsapp-icon"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/vanessa-kasei-ba91b8237/" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-linkedin" id="linkedin-icon"></i>
+                </a>
+                <a href="https://twitter.com/coderonX" target="_blank" rel="noopener noreferrer">
+                    <i className="fa fa-twitter" id="twitter-icon"></i>
                 </a>
             </div>
             <div className='contact-container'>
-                <p id='contact'>CONTACT US TODAY:  
-                <i className='fa fa-phone' id='phone'>  +254 701353 971</i>
-                <i className='fa fa-envelope' id='mail'> :mwayiupright@gmail.com</i></p>
+                <p id='contact'>Contact me on:  
+                <i className='fa fa-phone' id='phone'>  +254 710705715</i></p>
+            <p>Looking forward to Hearing from you and help you find your dream home!</p>
             </div>
-        </div>
+        </section>
     );
 }
 
